34a64e7b1a777b113b366c1ab18c0dadf7f6338b
varunmahajan292 <52626991+varunmahajan292@users.noreply.github.com>
2019-07-07 09:14:30 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,14 @@
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c;
+        c=a+b;
+        System.out.println("The value of a+b in c is"+"  =  "+c);
+    }
+    
+}
