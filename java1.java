98f9da6132d801522ab71f3076796e9bba2c9d91
varunmahajan292 <52626991+varunmahajan292@users.noreply.github.com>
2019-07-07 09:27:30 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -4,11 +4,11 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a=5;
-        int b=6;
+        int a=135;
+        int b=126;
         int c;
         c=a+b;
-        System.out.println("The value of a+b in c is"+"  =  "+c);
+        System.out.println("The value of a+b in c after ammend"+"  =  "+c);
     }
     
 }
